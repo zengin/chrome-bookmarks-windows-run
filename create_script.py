8b243ea98d8935295185861554804f37eaf1d6ef
Mustafa Zengin <mzengin88@gmail.com>
2016-11-18 00:51:49 -0800

    Add optional browser parameter

--- create_script.py
+++ create_script.py
@@ -1,18 +1,21 @@
-def create_script_string(bookmarks):
+def create_script_string(bookmarks, browser = "chrome"):
 	if len(bookmarks) is 0:
 		return ""
-		
+
+	browser_cmd_map = {"edge" : "microsoft-edge:", "firefox": "firefox ", "chrome" : "chrome ", "ie": "iexplore "}
+	browser_cmd = browser_cmd_map[browser]
+	
 	file_start = '''GOTO :URL_%1
 exit
 '''
 
 	goto_format_string = ''':URL_{}
-start chrome {}
+start {}{}
 exit
 '''
 	
 	script = file_start
 	for name, url in bookmarks.iteritems():
-		script += goto_format_string.format(name, url)
+		script += goto_format_string.format(name, browser_cmd, url)
 	
 	return script
--- main.py
+++ main.py
@@ -3,15 +3,20 @@ import create_script as cs
 import sys
 
 def show_usage():
-	print "usage: python main.py <folder_name>"
+	print '''usage: python main.py <folder_name> <browser>
+<browser> can be edge firefox ie or chrome'''
 
 if __name__ == "__main__":
-	if len(sys.argv) is not 2:
+	if len(sys.argv) < 2:
 		show_usage()
 	else:
 		file_path = sys.argv[1] + "\\b.bat"
 		bookmarks = gb.get_bookmark_dict()
-		script = cs.create_script_string(bookmarks)
+		script = ""
+		if len(sys.argv) is 3:
+			script = cs.create_script_string(bookmarks, sys.argv[2])
+		else:
+			script = cs.create_script_string(bookmarks)
 		output = open(file_path, "w")
 		output.write(script)
 		output.close()
